f969875ec099aeeb84dcdf95837d10ac9291c40c
Karenina-na <weizixiang0@outlook.com>
2023-03-27 10:28:24 +0800

    加入混淆矩阵画图 plot_confusion_matrix.py

---
+++ plot/plot_confusion_matrix.py
@@ -0,0 +1,39 @@
+import itertools
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# 画混淆矩阵
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    - cm :  the value of the confusion matrix calculated
+    - classes : the column corresponding to each row and column in the confusion matrix
+    - normalize :   True: show percentage, False: show number
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("显示百分比：")
+        np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
+        print(cm)
+    else:
+        print('显示具体数字：')
+        print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    # matplotlib problem, if not add the following code, the confusion matrix can only be displayed half, some versions of matplotlib do not need the following code, try it separately
+    plt.ylim(len(classes) - 0.5, -0.5)
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
