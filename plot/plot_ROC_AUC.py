a196c9b574bb4af9d80bbbfc01ac9a08bb9d4a19
Karenina-na <weizixiang0@outlook.com>
2023-03-27 17:12:28 +0800

    ROC_AUC曲线画图工具

---
+++ plot/plot_ROC_AUC.py
@@ -0,0 +1,44 @@
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
+from itertools import cycle
+from sklearn.preprocessing import label_binarize
+
+
+# 画ROC_AUC曲线
+def ROC_AUC(test, output_prob, classes):
+    """
+    ROC AUC
+    :param test:    测试集标签
+    :param output_prob:     模型输出的概率
+    :param classes:     类别
+    :return:
+    """
+    # Binarize the output
+    y_test = label_binarize(test, classes=classes)
+    n_classes = y_test.shape[1]
+    y_score = output_prob
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    # Plot of a ROC curve for a specific class
+    plt.figure()
+    plt.plot(fpr["micro"], tpr["micro"],
+             label='micro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["micro"]))
+    plt.plot(fpr[2], tpr[2], color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Some extension of Receiver operating characteristic to multi-class')
+    plt.legend(loc="lower right")
+    plt.show()
