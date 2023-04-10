65a0fe56a385f03243984f9e751710a74689da88
Karenina-na <weizixiang0@outlook.com>
2023-04-10 18:59:48 +0800

    Transformer

---
+++ Net/Transformer/Transformer.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as Data
+import numpy as np
+from Coder.Encoder import Encoder
+from Coder.Decoder import Decoder
+
+
+class Transformer(nn.Module):
+    def __init__(self, src_vocab_size, tgt_vocab_size, d_model, n_layers,
+                d_ff, d_k, d_v, n_heads):
+        super(Transformer, self).__init__()
+        self.encoder = Encoder(src_vocab_size, d_model, n_layers, d_ff, d_k, d_v, n_heads)
+        self.decoder = Decoder(tgt_vocab_size, d_model, n_layers, d_ff, d_k, d_v, n_heads)
+        self.projection = nn.Linear(d_model, tgt_vocab_size, bias=False)
+
+    def forward(self, enc_inputs, dec_inputs):
+        """
+        enc_inputs: [batch_size, src_len]
+        dec_inputs: [batch_size, tgt_len]
+        """
+        # tensor to store decoder outputs
+        # outputs = torch.zeros(batch_size, tgt_len, tgt_vocab_size).to(self.device)
+
+        # enc_outputs: [batch_size, src_len, d_model], enc_self_attns: [n_layers, batch_size, n_heads, src_len, src_len]
+        enc_outputs, enc_self_attns = self.encoder(enc_inputs)
+        # dec_outputs: [batch_size, tgt_len, d_model], dec_self_attns: [n_layers, batch_size, n_heads, tgt_len, tgt_len], dec_enc_attn: [n_layers, batch_size, tgt_len, src_len]
+        dec_outputs, dec_self_attns, dec_enc_attns = self.decoder(dec_inputs, enc_inputs, enc_outputs)
+        dec_logits = self.projection(dec_outputs)  # dec_logits: [batch_size, tgt_len, tgt_vocab_size]
+        return dec_logits.view(-1, dec_logits.size(-1)), enc_self_attns, dec_self_attns, dec_enc_attns
+
+
+if __name__ == "__main__":
+    from MakeData_Test import LoadData, LoadTestData
+
+    loader, src_vocab_size, tgt_vocab_size = LoadData()
+
+    # Transformer Parameters
+    d_model = 512  # Embedding Size
+    d_ff = 2048  # FeedForward dimension
+    d_k = d_v = 64  # dimension of K(=Q), V
+    n_layers = 6  # number of Encoder of Decoder Layer
+    n_heads = 8  # number of heads in Multi-Head Attention
+
+    model = Transformer(src_vocab_size, tgt_vocab_size, d_model, n_layers,
+                        d_ff, d_k, d_v, n_heads)
+    criterion = nn.CrossEntropyLoss(ignore_index=0)
+    optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.99)
+
+    for epoch in range(30):
+        for enc_inputs, dec_inputs, dec_outputs in loader:
+            '''
+            enc_inputs: [batch_size, src_len]
+            dec_inputs: [batch_size, tgt_len]
+            dec_outputs: [batch_size, tgt_len]
+            '''
+            # enc_inputs, dec_inputs, dec_outputs = enc_inputs.to(device), dec_inputs.to(device), dec_outputs.to(device)
+            # outputs: [batch_size * tgt_len, tgt_vocab_size]
+            outputs, enc_self_attns, dec_self_attns, dec_enc_attns = model(enc_inputs, dec_inputs)
+            loss = criterion(outputs, dec_outputs.view(-1))
+            print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+
+    # Test
+    idx2word = LoadTestData()
+
+    enc_inputs, dec_inputs, _ = next(iter(loader))
+    predict, _, _, _ = \
+        model(enc_inputs[0].view(1, -1), dec_inputs[0].view(1, -1))  # model(enc_inputs[0].view(1, -1), greedy_dec_input)
+    predict = predict.data.max(1, keepdim=True)[1]
+    print(enc_inputs[0], '->', [idx2word[n.item()] for n in predict.squeeze()])
