11f28e60af59a083885785a131288a3d58069590
Karenina-na <weizixiang0@outlook.com>
2023-05-04 22:01:38 +0800

    ConvNeXt网络

---
+++ Models/ConvNeXt/ConvNeXt.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from Models.ConvNeXt.Module.ConvNeXtBlock import ConvNeXtBlock
+from Models.ConvNeXt.Module.LayerNorm import LayerNorm
+
+
+class ConvNeXt(nn.Module):
+    r""" ConvNeXt
+    Args:
+        in_chans (int): Number of input image channels. Default: 3
+        num_classes (int): Number of classes for classification head. Default: 1000
+        depths (tuple(int)): Number of blocks at each stage. Default: [3, 3, 9, 3]
+        dims (int): Feature dimension at each stage. Default: [96, 192, 384, 768]
+        drop_path_rate (float): Stochastic depth rate. Default: 0.
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+        head_init_scale (float): Init scaling value for classifier weights and biases. Default: 1.
+    """
+
+    def __init__(self, in_chans: int = 3, num_classes: int = 1000, depths: list = None,
+                 dims: list = None, drop_path_rate: float = 0., layer_scale_init_value: float = 1e-6,
+                 head_init_scale: float = 1.):
+        super().__init__()
+        self.downsample_layers = nn.ModuleList()  # stem and 3 intermediate downsampling conv layers
+        stem = nn.Sequential(nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
+                             LayerNorm(dims[0], eps=1e-6, data_format="channels_first"))
+        self.downsample_layers.append(stem)
+
+        # 对应stage2-stage4前的3个downsample
+        for i in range(3):
+            downsample_layer = nn.Sequential(LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
+                                             nn.Conv2d(dims[i], dims[i + 1], kernel_size=2, stride=2))
+            self.downsample_layers.append(downsample_layer)
+
+        self.stages = nn.ModuleList()  # 4 feature resolution stages, each consisting of multiple blocks
+        dp_rates = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
+        cur = 0
+        # 构建每个stage中堆叠的block
+        for i in range(4):
+            stage = nn.Sequential(
+                *[ConvNeXtBlock(dim=dims[i], drop_rate=dp_rates[cur + j], layer_scale_init_value=layer_scale_init_value)
+                  for j in range(depths[i])]
+            )
+            self.stages.append(stage)
+            cur += depths[i]
+
+        self.norm = nn.LayerNorm(dims[-1], eps=1e-6)  # final norm layer
+        self.head = nn.Linear(dims[-1], num_classes)
+        self.apply(self._init_weights)
+        self.head.weight.data.mul_(head_init_scale)
+        self.head.bias.data.mul_(head_init_scale)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
+            nn.init.trunc_normal_(m.weight, std=0.2)
+            nn.init.constant_(m.bias, 0)
+
+    def forward_features(self, x: torch.Tensor) -> torch.Tensor:
+        for i in range(4):
+            x = self.downsample_layers[i](x)
+            x = self.stages[i](x)
+
+        return self.norm(x.mean([-2, -1]))  # global average pooling, (N, C, H, W) -> (N, C)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
+
+
+if __name__ == "__main__":
+    model = ConvNeXt(depths=[3, 3, 9, 3],
+                     dims=[96, 192, 384, 768],
+                     num_classes=3)
+    x = torch.randn(1, 3, 224, 224)
+    y = model(x)
+    print(y.shape)
---
+++ Models/ConvNeXt/CreateModel.py
@@ -0,0 +1,40 @@
+def convnext_tiny(num_classes: int):
+    # https://dl.fbaipublicfiles.com/convnext/convnext_tiny_1k_224_ema.pth
+    model = ConvNeXt(depths=[3, 3, 9, 3],
+                     dims=[96, 192, 384, 768],
+                     num_classes=num_classes)
+    return model
+
+
+def convnext_small(num_classes: int):
+    # https://dl.fbaipublicfiles.com/convnext/convnext_small_1k_224_ema.pth
+    model = ConvNeXt(depths=[3, 3, 27, 3],
+                     dims=[96, 192, 384, 768],
+                     num_classes=num_classes)
+    return model
+
+
+def convnext_base(num_classes: int):
+    # https://dl.fbaipublicfiles.com/convnext/convnext_base_1k_224_ema.pth
+    # https://dl.fbaipublicfiles.com/convnext/convnext_base_22k_224.pth
+    model = ConvNeXt(depths=[3, 3, 27, 3],
+                     dims=[128, 256, 512, 1024],
+                     num_classes=num_classes)
+    return model
+
+
+def convnext_large(num_classes: int):
+    # https://dl.fbaipublicfiles.com/convnext/convnext_large_1k_224_ema.pth
+    # https://dl.fbaipublicfiles.com/convnext/convnext_large_22k_224.pth
+    model = ConvNeXt(depths=[3, 3, 27, 3],
+                     dims=[192, 384, 768, 1536],
+                     num_classes=num_classes)
+    return model
+
+
+def convnext_xlarge(num_classes: int):
+    # https://dl.fbaipublicfiles.com/convnext/convnext_xlarge_22k_224.pth
+    model = ConvNeXt(depths=[3, 3, 27, 3],
+                     dims=[256, 512, 1024, 2048],
+                     num_classes=num_classes)
+    return model
