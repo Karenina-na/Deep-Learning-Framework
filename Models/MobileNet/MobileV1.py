b8ebf39a4996f2ae7210c426aac42edfbb6bfa86
Karenina-na <weizixiang0@outlook.com>
2023-05-04 15:54:33 +0800

    MobileNet v1

---
+++ Models/MobileNet/MobileV1.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+import torchvision
+from Models.Block.DeepwiseSeparableConv_V1_2d import BottleneckV1
+
+
+class MobileNetV1(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(MobileNetV1, self).__init__()
+
+        self.first_conv = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU6(inplace=True),
+        )
+
+        self.bottleneck = nn.Sequential(
+            BottleneckV1(32, 64, stride=1),
+            BottleneckV1(64, 128, stride=2),
+            BottleneckV1(128, 128, stride=1),
+            BottleneckV1(128, 256, stride=2),
+            BottleneckV1(256, 256, stride=1),
+            BottleneckV1(256, 512, stride=2),
+            BottleneckV1(512, 512, stride=1),
+            BottleneckV1(512, 512, stride=1),
+            BottleneckV1(512, 512, stride=1),
+            BottleneckV1(512, 512, stride=1),
+            BottleneckV1(512, 512, stride=1),
+            BottleneckV1(512, 1024, stride=2),
+            BottleneckV1(1024, 1024, stride=1),
+        )
+
+        self.avg_pool = nn.AvgPool2d(kernel_size=7, stride=1)
+        self.linear = nn.Linear(in_features=1024, out_features=num_classes)
+        self.dropout = nn.Dropout(p=0.2)
+        self.softmax = nn.Softmax(dim=1)
+
+        self.init_params()
+
+    def init_params(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear) or isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.first_conv(x)
+        x = self.bottleneck(x)
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.linear(x)
+        out = self.softmax(x)
+        return out
+
+
+if __name__ == '__main__':
+    model = MobileNetV1(3)
+    x = torch.randn(1, 3, 224, 224)
+    out = model(x)
+    print(out.shape)
