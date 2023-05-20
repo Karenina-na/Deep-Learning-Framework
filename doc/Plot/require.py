d5c9b7f119cea8c7462c782f7fc383e1306f460c
Karenina-na <weizixiang0@outlook.com>
2023-05-20 16:37:56 +0800

    增加画图样例

---
+++ doc/Plot/require.py
@@ -0,0 +1,33 @@
+# 导入依赖
+
+# https://www.machinelearningplus.com/plots/top-50-matplotlib-visualizations-the-master-plots-python
+
+import warnings
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+warnings.filterwarnings(action='once')
+
+large = 22
+medium = 16
+small = 12
+params = {'axes.titlesize': large,
+          'legend.fontsize': medium,
+          'figure.figsize': (16, 10),
+          'axes.labelsize': medium,
+          'xtick.labelsize': medium,
+          'ytick.labelsize': medium,
+          'figure.titlesize': large}
+plt.rcParams.update(params)
+plt.style.use('seaborn-whitegrid')
+sns.set_style("white")
+
+print(np.__version__)
+print(pd.__version__)
+print(mpl.__version__)
+print(sns.__version__)
