a1bfd9948a7417270f75bcad2d661a6a3935027d
Karenina-na <weizixiang0@outlook.com>
2023-06-07 09:50:30 +0800

    Add VGG 1d net

--- Models/VGG/VGG_2d.py
+++ Models/VGG/VGG_2d.py
@@ -8,7 +8,7 @@ class VGG(nn.Module):
         self.features = features  # 卷积层提取特征
         self.classifier = nn.Sequential(  # 全连接层进行分类
             nn.Dropout(p=0.5),
-            nn.Linear(512 * 7, 2048),
+            nn.Linear(512 * 7 * 7, 2048),
             nn.ReLU(True),
             nn.Dropout(p=0.5),
             nn.Linear(2048, 2048),
@@ -19,10 +19,11 @@ class VGG(nn.Module):
             self._initialize_weights()  # 初始化权重
 
     def forward(self, x):
+        # N x 3 x 224 x 224
         x = self.features(x)
-        # N x 512 x 7
+        # N x 512 x 7 x 7
         x = torch.flatten(x, start_dim=1)
-        # N x 512*7
+        # N x 512*7*7
         x = self.classifier(x)
         return x
 
@@ -50,34 +51,34 @@ cfgs = {
 
 
 # 卷积层提取特征
-def make_features(cfg: list, channel):  # 传入的是具体某个模型的参数列表
+def make_features(cfg: list):  # 传入的是具体某个模型的参数列表
     layers = []
-    in_channels = channel  # 输入的原始图像(rgb三通道)
+    in_channels = 3  # 输入的原始图像(rgb三通道)
     for v in cfg:
         # 如果是最大池化层，就进行池化
         if v == "M":
-            layers += [nn.MaxPool1d(kernel_size=2, stride=2)]
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
         # 不然就是卷积层
         else:
-            conv2d = nn.Conv1d(in_channels, v, kernel_size=3, padding=1)
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
             layers += [conv2d, nn.ReLU(True)]
             in_channels = v
     return nn.Sequential(*layers)  # 单星号(*)将参数以元组(tuple)的形式导入
 
 
-def vgg(model_name="vgg16", channel=3, **kwargs):  # 双星号(**)将参数以字典的形式导入
+def vgg(model_name="vgg16", **kwargs):  # 双星号(**)将参数以字典的形式导入
     try:
         cfg = cfgs[model_name]
     except:
         print("Warning: model number {} not in cfgs dict!".format(model_name))
         exit(-1)
-    model = VGG(make_features(cfg, channel), **kwargs)  # **kwargs是你传入的字典数据
+    model = VGG(make_features(cfg), **kwargs)  # **kwargs是你传入的字典数据
     return model
 
 
 if __name__ == '__main__':
-    model = vgg('vgg16', channel=2, num_classes=2, init_weights=True)
-    print(model)
-    input = torch.randn(32, 2, 224)
-    out = model(input)
-    print(out.shape)
+    vgg16 = vgg('vgg16', num_classes=10, init_weights=True)
+    print(vgg16)
+    input = torch.randn(1, 3, 224, 224)
+    output = vgg16(input)
+    print(output.shape)
