038a7ffc656223b28d967438e1950adc417c9d1c
Karenina-na <weizixiang0@outlook.com>
2023-07-05 19:29:13 +0800

    update plot_confusion_matrix.py

--- util/plot/plot_confusion_matrix.py
+++ util/plot/plot_confusion_matrix.py
@@ -1,7 +1,7 @@
 import itertools
-import numpy as np
-import pandas as pd
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # 画混淆矩阵
@@ -11,6 +11,7 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     - classes : the column corresponding to each row and column in the confusion matrix
     - normalize :   True: show percentage, False: show number
     """
+    plt.figure(dpi=300)
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         print("显示百分比：")
@@ -23,7 +24,7 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     plt.title(title)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
+    plt.xticks(tick_marks, classes)
     plt.yticks(tick_marks, classes)
     # matplotlib problem, if not add the following code, the confusion matrix can only be displayed half, some versions of matplotlib do not need the following code, try it separately
     plt.ylim(len(classes) - 0.5, -0.5)
@@ -32,8 +33,18 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         plt.text(j, i, format(cm[i, j], fmt),
                  horizontalalignment="center",
+                 verticalalignment='center',
                  color="white" if cm[i, j] > thresh else "black")
     plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
     plt.show()
+
+
+if __name__ == "__main__":
+    classes = [0, 1, 2, 3, 4, 5]
+    cm = np.array([[10, 0, 0, 0, 0, 0],
+                   [0, 10, 0, 0, 0, 0],
+                   [0, 0, 10, 0, 0, 0],
+                   [0, 0, 0, 10, 0, 0],
+                   [0, 0, 0, 0, 10, 0],
+                   [0, 0, 0, 0, 0, 10]])
+    plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues)
