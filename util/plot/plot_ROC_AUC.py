33379fe31a755888f1a36b1b1f3d08348655120f
Karenina-na <weizixiang0@outlook.com>
2023-07-09 17:15:51 +0800

    二分类ROC曲线

--- util/plot/plot_ROC_AUC.py
+++ util/plot/plot_ROC_AUC.py
@@ -44,3 +44,71 @@ def ROC_AUC(test, output_prob, classes):
     plt.title('Some extension of Receiver operating characteristic to multi-class')
     plt.legend(loc="lower right")
     plt.show()
+
+from matplotlib import pyplot as plt
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+
+
+# 画ROC_AUC曲线
+def ROC_AUC(test, output_prob, classes):
+    """
+    ROC AUC
+    :param test:    测试集
+    :param output_prob:     模型输出的概率
+    :param classes:     类别
+    :return:
+    """
+    lw = 2
+    # Binarize the output
+    y_test = label_binarize(test, classes=classes)
+    n_classes = y_test.shape[1]
+    y_score = output_prob
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    # Plot of a ROC curve for a specific class
+    plt.figure(dpi=300)
+    plt.plot(fpr["micro"], tpr["micro"],
+             label='micro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["micro"]))
+    plt.plot(fpr[2], tpr[2], color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Some extension of Receiver operating characteristic to multi-class')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+def ROC_AUC_Signal(test, output_prob):
+    """
+    ROC AUC for 二分类
+    :param test:   测试集
+    :param output_prob:  模型输出的概率
+    :return:
+    """
+    fpr, tpr, thresholds = roc_curve(test, output_prob[:, 1])
+    roc_auc = auc(fpr, tpr)
+    plt.figure(dpi=300)
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw,
+             label='ROC curve (area = %0.4f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
